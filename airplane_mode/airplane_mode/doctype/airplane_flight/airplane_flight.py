bfb971f9011630d04f337b2b7de7575aaf0c0306
Rohit Gunjegaonkar <rohit.g@promantia.com>
2023-06-20 17:22:48 +0530

    day 2 handout chnages

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -2,9 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
 
-class AirplaneFlight(Document):
+from frappe.website.website_generator import WebsiteGenerator
+
+class AirplaneFlight(WebsiteGenerator):
     
  def before_submit(self):
      self.status = 'Completed'
