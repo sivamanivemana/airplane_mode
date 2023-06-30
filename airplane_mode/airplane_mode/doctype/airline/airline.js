223ea941a96166663a5289481835b9485c6b8c94
Rohit Gunjegaonkar <rohit.g@promantia.com>
2023-06-30 17:30:25 +0530

    changes in airline for web link

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -3,6 +3,11 @@
 
 frappe.ui.form.on('Airline', {
 	refresh: function(frm) {
-			console.log(frm.doc.route)
+		console.log(frm.doc.website)
+		if (frm.doc.website){
+			cur_frm.add_web_link(frm.doc.website, "Visit Website")
+		}else{
+			
+		}
 	}
 });
