3c9ad3e07a9c9adff5889e5c67b3e69545cad6e3
Rohit Gunjegaonkar <rohit.g@promantia.com>
2023-06-30 13:23:20 +0530

    changes

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Airline', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+			console.log(frm.doc.route)
+	}
 });
